804b54619fc06ec323d0877b8fb2959e392953a4
chuaimen <chuaimen@163.com>
2025-07-20 18:32:55 +0800

    创建 一个修改模板 快速应用

---
+++ djangooRecorder/ShunFeng/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import ShunFengPost
+
+# Register your models here.
+# 注册新的数据库
+
+admin.site.register(ShunFengPost)
---
+++ djangooRecorder/ShunFeng/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+#修改 数据库名称
+class ShunFengPost(models.Model):
+    client = models.CharField(max_length=75)
+    workduty = models.TextField()
+    # slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='fallback.png', blank=True)
+
+    def __str__(self):
+        return self.client
+
+
